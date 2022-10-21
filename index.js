e3bc9b687f5b643b88a860c95b324fd0681c2664
Sotateck-TungPham <tung.pham@sotateck.com>
2022-10-21 09:02:47 +0700

    merge file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load('./api-bff.yaml');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
